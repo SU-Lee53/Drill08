bf8deddda67ff8a2fa9da76adea25fca76e54a26
SU-Lee53 <dltmddnr05@naver.com>
2023-10-05 10:38:15 +0900

    잔디 클래스, 객체 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,15 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image(('grass.png'))
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self): pass
+
 
 def handle_events():
     global running
@@ -13,15 +22,20 @@ def handle_events():
 
 def reset_world():
     global running
+    global grass
+
     running = True
+    grass = Grass()
 
 
 def update_world():
+    grass.update()
     pass
 
 
 def render_world():
     clear_canvas()
+    grass.draw()
     update_canvas()
 
 
@@ -36,7 +50,7 @@ while running:
     handle_events()
     update_world()
     render_world()
-
+    delay(0.05)
 
 
 # finalization code
